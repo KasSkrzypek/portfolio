e1cb4e51b1354430f2af834745842c9f90be7b08
KasSkrzypek <97444889+KasSkrzypek@users.noreply.github.com>
2022-02-03 17:52:08 +0100

    add hamburger menu script

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,18 @@
-console.log("Welcome to my portfolio site")
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
+/*
+function scrollToAnchor(aid){
+  const destination = $("a[name='"+ aid +"']");
+    $('html,body').animate({
+      scrollTop: destination.offset().top
+    },'slow');
+}
+$(document).on('click', '.smooth-link', function(){
+  scrollToAnchor('contact');
+})*/

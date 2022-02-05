f6189a469a7304bca9b05e2e416703bcbd6b6b39
KasSkrzypek <97444889+KasSkrzypek@users.noreply.github.com>
2022-02-05 12:42:51 +0100

    update script

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,33 @@
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
-  if (x.className === 'navtoggle') {
+  if (x.className === 'navtoggle' && $(window).width() < 640) {
     x.className += ' responsive';
   } else {
     x.className = 'navtoggle';
   }
 }
-/*
-function scrollToAnchor(aid){
-  const destination = $("a[name='"+ aid +"']");
-    $('html,body').animate({
-      scrollTop: destination.offset().top
-    },'slow');
-}
-$(document).on('click', '.smooth-link', function(){
-  scrollToAnchor('contact');
-})*/
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});

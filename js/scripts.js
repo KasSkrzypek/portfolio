0aa3e96d7027eaf8294109569b3382d4c5e19402
KasSkrzypek <97444889+KasSkrzypek@users.noreply.github.com>
2022-02-18 10:30:30 +0100

    update image modal

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
+AOS.init({
+  duration: 1200,
+})
+
+
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle' && $(window).width() < 640) {
@@ -52,4 +57,26 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
 }
